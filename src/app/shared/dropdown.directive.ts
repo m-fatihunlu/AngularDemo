a104bff39a72cdee57723f6f4e23c9d75b053a0e
Fatih ÜNLÜ <fatih.unlu@multinetinventiv.com>
2020-06-03 17:41:37 +0300

    clicking outside with directive

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,21 @@
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  HostBinding,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropDownDirective {
+  @HostBinding('class.open') isOpen = false; // class is array of all classes that element have.
+
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
+  }
+
+  constructor(private elRef: ElementRef) {}
+}
